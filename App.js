450daff37a023930a51c8d2c2718e931ac65d7e7
wonjangcloud9 <venuscwj@naver.com>
2023-05-17 22:50:05 +0900

    useState & useEffect

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    console.log("did mount");
+  }, []);
+
+  useEffect(() => {
+    console.log("did update");
+  }, [data]);
+
+  const click = () => {
+    console.log("Hi");
+    setData(data + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          backgroundColor: "black",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {data}
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Button title="Hi" onPress={click} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          backgroundColor: "red",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
+          stlye={{
+            backgroundColor: "black",
+          }}
+          onPress={click}
+        >
+          <Text>Hello world</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +72,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
   },
 });
